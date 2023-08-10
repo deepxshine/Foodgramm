be406be0b302cba7407d556bb13094702dd46bf8
deepxshine <aevdok2800@gmail.com>
2023-08-10 15:14:18 +0300

    000

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,16 +4,16 @@ from rest_framework import routers
 from .views import (IngredientsViewSet, TagViewSet, RecipeViewSet, UsersViewSet,FavoriteRecipeViewSet)
 
 router = routers.DefaultRouter()
-# router.register(
-#     r'recipes/(?P<recipe_id>\d+)/favorite',
-#     FavoriteRecipeViewSet,
-#     basename='favorite'
-# )
-# router.register(
-#     r'recipes/(?P<recipe_id>\d+)/shopping_cart',
-#     FavoriteRecipeViewSet,
-#     basename='favorite'
-# )
+router.register(
+    r'recipes/(?P<recipe_id>\d+)/favorite',
+    FavoriteRecipeViewSet,
+    basename='favorite'
+)
+router.register(
+    r'recipes/(?P<recipe_id>\d+)/shopping_cart',
+    FavoriteRecipeViewSet,
+    basename='favorite'
+)
 router.register('ingredients', IngredientsViewSet)
 router.register('users', UsersViewSet)
 router.register('tags', TagViewSet)
